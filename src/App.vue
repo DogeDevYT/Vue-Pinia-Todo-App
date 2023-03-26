2b4b3c52b69a587316744c67477ef9162ac09cb6
DogeDevYT <52612977+DogeDevYT@users.noreply.github.com>
2023-03-26 10:24:25 -0400

    Initial Commit
    commiting

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<script setup>
+import TodoApp from './components/TodoApp.vue';
+</script>
+
+<template>
+  <todo-app />
+</template>
+
+<style>
+#app {
+  font-family: Arial, Helvetica, sans-serif;
+  text-align: center;
+  color: #2c3e50;
+}
+</style>
